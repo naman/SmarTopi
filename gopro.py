71e4aea924a48747df9d1e85daa98b798ff94b42
Udayan Tandon <udayan12167@iiitd.ac.in>
2016-11-29 02:51:35 +0530

    facerecognition structuring

--- gopro.py
+++ gopro.py
@@ -9,8 +9,8 @@ from bs4 import BeautifulSoup
 import subprocess
 from fbrecog import recognize
 
-access_token = "EAACEdEose0cBAF2LiQE6mNG8ixIvblmwHwlhAwjo7tOas1ahfXlGUpLsJkQWq5iq3ENkEojFtKC5xEUSQs$
-cookie = "datr=fPqQVfDF5cEMKwcywPvt70jN; _ga=GA1.2.1788536487.1437330029; locale=en_GB; sb=_U0HV7uS$
+access_token = "EAACEdEose0cBAEpbrjlnvFZBLemSZAwZBJ2eP1Jcboqn5ftbedBjVzd9CMFM175ZApaZBZA7IVo7zZAh9neUQuoc46ogOQb81ZCpSQsfq3ZBg7UOO1LdxT5TVPzUDyAlZA80pg4RB6VHNoddcLL7LA2thfwADDY11H4rZBZCDYjItQeYLAZDZD"
+cookie = "datr=fPqQVfDF5cEMKwcywPvt70jN; _ga=GA1.2.1788536487.1437330029; locale=en_GB; sb=_U0HV7uSQVX_Z8Oov3bZav9c; pl=n; lu=giTXKkQ81PDlziOlnp5_5J7A; c_user=100000601825742; xs=204%3As6GCwRc3trP-_w%3A2%3A1479817217%3A5827; fr=0J8CbTl5k918hSbLX.AWWGwk7QSJhzwUuSO-iL8J2fpEo.BVkPqC.Nv.Fg0.0.0.BYPH2T.AWXxfm--; csm=2; s=Aa7PrE1w24FGNzCh.BYNDgB; p=-2; presence=EDvF3EtimeF1480361527EuserFA21B00601825742A2EstateFDt2F_5b_5dElm2FnullEuct2F1480325937BEtrFA2loadA2EtwF2218223787EatF1480361522186G480361527040CEchFDp_5f1B00601825742F15CC; act=1480361603300%2F10; wd=1466x536"
 fb_dtsg = "AQGQIXXXTjK_:AQH9HojIEPNb"
 
 
@@ -77,12 +77,17 @@ for f in os.listdir("."):
 print "Recognition results"
 print lol
 
-fb = []
+fb = {}
 for f in os.listdir("."):
-    if f.endswith(".jpg"):
-	fb.append(recognize(f,access_token,cookie,fb_dtsg))
+	if f.endswith(".jpg"):
+		for item in recognize(f,access_token,cookie,fb_dtsg):
+			if(item['certainity']>0.8):
+				if f in fb:
+					fb[f].append(item['name'])
+				else:
+					fb[f] = [item['name']]
 
-print "Facebook Recognition"
-print fb
+print("Facebook Recognition")
+print(fb)
 
-print "Check timeseries plot on any client."
+print("Check timeseries plot on any client.")
